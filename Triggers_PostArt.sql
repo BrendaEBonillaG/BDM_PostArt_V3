8b7ddd44869f80c0574f53fc3be985d20c310a8f
BrendaEBonillaG <82917836+BrendaEBonillaG@users.noreply.github.com>
2025-05-22 11:06:41 -0600

    triggers arreglados

--- Triggers_PostArt.sql
+++ Triggers_PostArt.sql
@@ -1,19 +1,39 @@
 -- TRIGGERS
 USE PostArt;
 
+
+DELIMITER $$
+
 CREATE TRIGGER suspender_publicaciones
 AFTER UPDATE ON Usuario
 FOR EACH ROW
 BEGIN
   IF NEW.Estado = 'Suspendido' THEN
-    UPDATE Publicaciones SET Estado = 'Inactivo' WHERE Id_usuario = NEW.ID_Usuario;
-    UPDATE Comentarios SET Estado = 'Inactivo' WHERE Id_usuario = NEW.ID_Usuario;
-  END IF;
-END;
+    UPDATE Publicaciones 
+    SET Estado = 'Inactivo' 
+    WHERE Id_usuario = NEW.ID_Usuario;
 
-CREATE TRIGGER eliminar_seguidores
-AFTER DELETE ON Usuario
+    UPDATE Comentarios 
+    SET Estado = 'Inactivo' 
+    WHERE Id_usuario = NEW.ID_Usuario;
+  END IF;
+END$$
+
+DELIMITER ;
+
+DELIMITER $$
+
+CREATE TRIGGER desactivar_seguidores
+AFTER UPDATE ON Usuario
 FOR EACH ROW
 BEGIN
-  DELETE FROM Seguidores WHERE Id_usuario_seguidor = OLD.ID_Usuario OR Id_usuario_artista = OLD.ID_Usuario;
-END;
+  IF NEW.Estado = 'Inactivo' OR NEW.Estado = 'Suspendido' THEN
+    UPDATE Seguidores 
+    SET Estado = 'Inactivo'
+    WHERE Id_usuario_seguidor = NEW.ID_Usuario 
+       OR Id_usuario_artista = NEW.ID_Usuario;
+  END IF;
+END$$
+
+DELIMITER ;
+
