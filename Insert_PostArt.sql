e3072202393dfcfb4f7f5bd77bf3fdeb709b1021
BrendaEBonillaG <82917836+BrendaEBonillaG@users.noreply.github.com>
2025-06-05 00:33:23 -0600

    inserts

---
+++ Insert_PostArt.sql
@@ -0,0 +1,13 @@
+USE PostArt;
+
+INSERT INTO Categorias (Nombre) VALUES
+('Ilustración'),
+('Pintura Digital'),
+('Arte Tradicional'),
+('Fotografía'),
+('Diseño Gráfico'),
+('3D'),
+('Animación'),
+('Comic'),
+('Fanart'),
+('Concept Art');
